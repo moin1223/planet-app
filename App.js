90f99c0ffaa3bb962f0d1db3a8fb1eeae53d37a6
moin1223 <moinpuccse@gmail.com>
2022-05-23 15:31:07 +0600

    Design system done

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { colors } from './src/theme/colors';
+import { spacing } from './src/theme/spacing';
+import { useFonts } from 'expo-font';
+import { typography } from './src/theme/typography';
+import Text from './src/components/text/text';
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text preset='h1' style={{color: "blue"}}>Open up App.js to start working on your app!</Text>
+      <Text style={{marginTop:spacing[8]}}>Hello World!</Text>
+      <Text style={{ fontFamily:typography.primary}}>Hello React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +32,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "black"
   },
 });
